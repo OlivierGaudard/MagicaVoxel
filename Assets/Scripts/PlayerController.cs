8fc0294085dccd83b8e2b865d755360840424b2d
OlivierGaudard <olivier.gaudard@gmail.com>
2022-01-15 04:45:49 +0100

    Blend Tree character animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float playerSpeed = 2.0f;
+    private float walkSpeed = 1.0f;
+    [SerializeField]
+    private float runSpeed = 3.0f;
     [SerializeField]
     private float jumpHeight = 1.0f;
     [SerializeField]
@@ -19,48 +21,92 @@ public class PlayerController : MonoBehaviour
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private Transform cameraTransform;
+    private float playerSpeed = 0f;
 
 
     private InputAction moveAction;
     private InputAction jumpAction;
+    private InputAction runAction;
+
+    private Animator anim;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
+        anim = GetComponent<Animator>();
         cameraTransform = Camera.main.transform;
         moveAction = playerInput.actions["Move"];
         jumpAction = playerInput.actions["Jump"];
+        runAction = playerInput.actions["Run"];
+
     }
 
     void Update()
     {
+
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
+
+
         Vector2 input = moveAction.ReadValue<Vector2>();
         Vector3 move = new Vector3(input.x, 0, input.y);
         move = move.x * cameraTransform.right.normalized + move.z * cameraTransform.forward.normalized;
         move.y = 0f;
-        controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero)
+        if(Input.GetKey(KeyCode.LeftShift) && move != Vector3.zero)
         {
+            Run();
             gameObject.transform.forward = move;
         }
+        else if(!Input.GetKey(KeyCode.LeftShift) && move != Vector3.zero)
+        {
+            Walk();        
+            gameObject.transform.forward = move;
+        }
+        else
+        {
+            Idle();
+        }
+        controller.Move(move * Time.deltaTime * playerSpeed);
 
         // Changes the height position of the player..
         if (jumpAction.triggered && groundedPlayer)
         {
+            Jump();
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            
         }
+        
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
 
         Quaternion targetRotation = Quaternion.Euler(0,cameraTransform.eulerAngles.y,0);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+        anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        playerSpeed = walkSpeed;
+        anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+    }
+    private void Run()
+    {
+        playerSpeed = runSpeed;
+        anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        anim.SetTrigger("Jump");
     }
 }
